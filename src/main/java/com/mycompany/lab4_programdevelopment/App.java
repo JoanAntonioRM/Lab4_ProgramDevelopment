4c1fcae21b25bbeec61cbaeb655c2f26abc11ae1
2533309 <2533309@D21013-JS3XM34.VanierCollege.intra>
2026-09-14 09:31:01 -0400

    Create TextFields

--- src/main/java/com/mycompany/lab4_programdevelopment/App.java
+++ src/main/java/com/mycompany/lab4_programdevelopment/App.java
@@ -3,6 +3,7 @@ package com.mycompany.lab4_programdevelopment;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,7 +15,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-
+        TextField numDays = new TextField();
+        TextField amountAirfare = new TextField();
+        TextField amountCarFees = new TextField();
+        TextField numMilesDriven = new TextField();
+        TextField amountParkingFees = new TextField();
+        TextField amountTaxiFees = new TextField();
+        TextField registrationFees = new TextField();
+        TextField lodgingCharges = new TextField();
     }
 
     public static void main(String[] args) {
