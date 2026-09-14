ad0a02d793cb5d7c4df5acc4491b43b5bbe73520
2533309 <2533309@D21013-JS3XM34.VanierCollege.intra>
2026-09-14 10:14:55 -0400

    Calculate Reimburse and Expenses

--- src/main/java/com/mycompany/lab4_programdevelopment/App.java
+++ src/main/java/com/mycompany/lab4_programdevelopment/App.java
@@ -12,6 +12,11 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
+    public static double MEALS_REIMBURSE = 37.00;
+    public static double PARKING_REIMBURSE = 10.00;
+    public static double TAXI_REIMBURSE = 20.00;
+    public static double LODGING_REIMBURSE = 95.00;
+    public static double MILES_REIMBURSE = 0.27;
 
     @Override
     public void start(Stage stage) {
@@ -32,6 +37,15 @@ public class App extends Application {
         Double taxiFees = Double.valueOf(amountTaxiFees.getText());
         Double registrationFees = Double.valueOf(amountRegistrationFees.getText());
         Double lodgingCharges = Double.valueOf(amountLodgingCharges.getText());
+        
+        Double parkingFeesReimburse = (parkingFees < PARKING_REIMBURSE)? PARKING_REIMBURSE - parkingFees : PARKING_REIMBURSE;
+        Double taxiFeesReimburse = (taxiFees < TAXI_REIMBURSE)? TAXI_REIMBURSE - taxiFees : TAXI_REIMBURSE;
+        Double lodgingChargesReimburse = (lodgingCharges < LODGING_REIMBURSE)? LODGING_REIMBURSE - lodgingCharges : LODGING_REIMBURSE;
+        Double totalReimburse = (MEALS_REIMBURSE * days) + (MILES_REIMBURSE * milesDriven) + lodgingChargesReimburse + taxiFeesReimburse + parkingFeesReimburse;
+        
+        Double totalExpenses = airfare + carFees + parkingFees + taxiFees + registrationFees + (lodgingCharges * days);
+        
+        Double excessExpenses = totalExpenses - totalReimburse;
     }
 
     public static void main(String[] args) {
